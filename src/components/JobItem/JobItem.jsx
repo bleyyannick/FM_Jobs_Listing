47c9db199cab2836e64a4f74668336b5eb36ce2c
Yannick Bley <bleyyannickpro@gmail.com>
2024-01-23 17:34:48 +0100

    feat: render statically data

--- src/components/JobItem/JobItem.jsx
+++ src/components/JobItem/JobItem.jsx
@@ -4,8 +4,32 @@
 function JobItem({jobItem}) {
   return (
     <article>
-     <div>
+     <div className="logo-img">
         <img src={jobItem.logo}/>
+     </div>
+     <div className="job-description">
+       <div className="job-title">
+        <h3>{jobItem.company}</h3>
+        {jobItem.new && <span>new</span>}
+        {jobItem.featured && <span>featured</span>}
+       </div>
+       <p>{jobItem.position}</p>
+     </div>
+     <div>
+      <span>{jobItem.postedAt}</span>
+      <ul>
+        <li>{jobItem.contract}</li>
+        <li>{jobItem.location}</li>
+      </ul>
+     </div>
+     <div>
+       {jobItem.languages}
+       {jobItem.role}
+       {jobItem.level}
+       {jobItem.tools}
+
+
+
      </div>
     </article>
   )
--- src/components/JobListItems/JobListItems.jsx
+++ src/components/JobListItems/JobListItems.jsx
@@ -2,5 +2,8 @@
 import styles from './JobListItems.module.css'; 
 
 
-export const JobListItems = ({children}) => <section className={styles.container}>{children}</section>; 
+export const JobListItems = ({children}) => 
+    <section className={styles.container}>
+        {children}
+    </section>; 
 
