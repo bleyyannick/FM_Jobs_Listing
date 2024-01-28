cfc07065ef522f3eab507bf18cca9ce5341f924d
Yannick Bley <bleyyannickpro@gmail.com>
2024-01-28 18:51:21 +0100

    feat: get rid of unecessary useEffect for updating jobs list

--- src/App.jsx
+++ src/App.jsx
@@ -3,31 +3,22 @@ import './App.css'
 import data from '../data.json'; 
 import JobList from './components/JobList/JobList';
 import FilterBar from './components/FilterBar/FilterBar';
-import { useEffect, useState } from 'react';
+import {  useState } from 'react';
 
 function App() {
   const [filters, setFilters] = useState([]); 
   const [isFiltered, setIsFiltered] = useState(false); 
-  const [filteredJobs, setFilteredJobs] = useState([]); 
-
-
-  useEffect( () => {
+  
     const updateJobList = (itemList, filtersList)=> { 
 
-     const copiedJobList = [...itemList];
+    const copiedJobList = [...itemList];
 
     const filteredItemList = filtersList.length <= 0 ? copiedJobList
                             : copiedJobList.filter(job =>
                                filtersList.every(filter =>
                                 [...job.languages, ...job.tools, job.role, job.level].includes(filter))); 
-
-      return filteredItemList
+        return filteredItemList
      }
-    setFilteredJobs(updateJobList(data,filters));
-
-  }, [filters])
-
-
    
   /**
    * 
@@ -42,7 +33,7 @@ function App() {
    * @param {string} filter 
    */
   const handleDeleteFilter = (filterToDelete) => {
-   return setFilters((prevFilters) => [...prevFilters].filter(filter => filter !== filterToDelete ));
+   setFilters((prevFilters) => [...prevFilters].filter(filter => filter !== filterToDelete ));
   }
 
 
@@ -51,7 +42,7 @@ function App() {
       {isFiltered && <FilterBar filters={filters} onDeleteFilter={handleDeleteFilter} />}
       <JobList
          onAddFilter={handleAddFilter}
-         jobs={filteredJobs} />
+         jobs={updateJobList(data, filters)} />
     </main>
   )
 }
