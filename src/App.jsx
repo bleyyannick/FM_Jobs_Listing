fb6c0eeae08d6efa18cd75ad2bed74f5a0d85438
Yannick Bley <bleyyannickpro@gmail.com>
2024-01-28 19:12:08 +0100

    chore: refactor code

--- src/App.jsx
+++ src/App.jsx
@@ -9,17 +9,6 @@ function App() {
   const [filters, setFilters] = useState([]); 
   const [isFiltered, setIsFiltered] = useState(false); 
   
-    const updateJobList = (itemList, filtersList)=> { 
-
-    const copiedJobList = [...itemList];
-
-    const filteredItemList = filtersList.length <= 0 ? copiedJobList
-                            : copiedJobList.filter(job =>
-                               filtersList.every(filter =>
-                                [...job.languages, ...job.tools, job.role, job.level].includes(filter))); 
-        return filteredItemList
-     }
-   
   /**
    * 
    * @param {string} filter 
@@ -28,13 +17,30 @@ function App() {
     setFilters((prevFilters) => [...prevFilters, filter])
     setIsFiltered(true); 
   }
+
   /**
    * 
-   * @param {string} filter 
+   * @param {string} filterToDelete
    */
   const handleDeleteFilter = (filterToDelete) => {
    setFilters((prevFilters) => [...prevFilters].filter(filter => filter !== filterToDelete ));
   }
+  /**
+   * 
+   * @param {object[]} itemList 
+   * @param {string[] | undefined} filtersList 
+   * @returns {object[]} 
+   */
+  const updateJobList = (itemList, filtersList) => { 
+    const copiedJobList = [...itemList];
+    
+    const filteredList = filtersList.length <= 0 ? copiedJobList
+                            : copiedJobList.filter(job =>
+                               filtersList.every(filter =>
+                                [...job.languages, ...job.tools, job.role, job.level].includes(filter))); 
+        return filteredList
+     }
+  const filteredJobList = updateJobList(data, filters); 
 
 
   return (
@@ -42,7 +48,7 @@ function App() {
       {isFiltered && <FilterBar filters={filters} onDeleteFilter={handleDeleteFilter} />}
       <JobList
          onAddFilter={handleAddFilter}
-         jobs={updateJobList(data, filters)} />
+         jobs={filteredJobList} />
     </main>
   )
 }
