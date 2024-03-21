c5cbb799b9a7e4ef8de502f363f9b9006f3f59d2
Yannick Bley <bleyyannickpro@gmail.com>
2024-03-21 08:09:23 +0100

    fix: deplace base

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/FM_Jobs_Listing/",
   plugins: [react()],
+  base: "/FM_Jobs_Listing/",
 });
